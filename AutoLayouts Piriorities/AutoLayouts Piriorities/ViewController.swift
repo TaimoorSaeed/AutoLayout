d636c26e7a11082d0440bd7ee7dc4d747ac0ecba
TaimoorSaeed <taimoordev@gmail.com>
2017-04-24 03:33:41 +0500

    added autolayouts pirioties exercies

---
+++ AutoLayouts Piriorities/AutoLayouts Piriorities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayouts Piriorities
+//
+//  Created by Admin on 24/04/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func buttonTapped(_ sender: UIButton ) {
+        sender.removeFromSuperview()
+    }
+
+
+}
+
